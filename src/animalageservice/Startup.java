ba538685ee0ed8c0d57dc25d61f9f0a52dfde841
User <User@192.168.0.7>
2017-09-24 10:51:17 -0500

    created a startup class to test code

--- src/animalageservice/Startup.java
+++ src/animalageservice/Startup.java
@@ -4,7 +4,12 @@ package animalageservice;
 public class Startup {
 
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        AnimalAgeCalculator animalAge = new CatAgeCalculator(10);       
+        AnimalAgeService animalAgeService = new AnimalAgeService(animalAge);
+        double humanAge = animalAgeService.calculateHumanAge(10);
+        
+        System.out.println("The human age equivalent of this animal is " + humanAge + " years.");
     }
     
 }
